62d49dffb28e316aff8cb8a250e06dc202fc0e24
white-hun <121800818+white-hun@users.noreply.github.com>
2023-03-27 18:11:24 +0900

    Update Products.jsx

--- react-query/src/components/Products.jsx
+++ react-query/src/components/Products.jsx
@@ -42,3 +42,7 @@ export default function Products() {
 
 // 네트워크 통신을 하는 곳에서 useQuery를 사용
 // useQuery를 사용할 때는 항상 key와 가지고오는 함수를 전달해줘야 한다
+// React Query 문서에서 API메뉴 useQuery에서 확인할 수 있다
+// key는 항상 고유한 값을 사용
+// 데이터를 어디서 가져와야하는지에 대한 함수(네트워크에서 데이터를 받아오는 로직)
+// promise든 await로 네트워크에서 받아온 데이터를 반환
